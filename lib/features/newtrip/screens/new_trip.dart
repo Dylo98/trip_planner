b1f8314b254c0628496675574c418099c65b365f
Dylo98 <dawid.dyl@microsoft.wsei.edu.pl>
2025-05-16 17:11:19 +0200

    Add example map

--- lib/features/newtrip/screens/new_trip.dart
+++ lib/features/newtrip/screens/new_trip.dart
@@ -20,21 +20,66 @@ class _NewTripScreenState extends State<NewTripScreen> {
     zoom: 12,
   );
 
+  final List<Marker> myMarker = [];
+  final List<Marker> markerList = const [
+    Marker(
+      markerId: MarkerId('First'),
+      position: LatLng(52.2297, 21.0122),
+      infoWindow: InfoWindow(title: 'My Position'),
+    ),
+    Marker(
+      markerId: MarkerId('Second'),
+      position: LatLng(52.2503, 21.0122),
+      infoWindow: InfoWindow(title: 'My Position 2'),
+    ),
+    Marker(
+      markerId: MarkerId('Third'),
+      position: LatLng(52.2503, 21.0622),
+      infoWindow: InfoWindow(title: 'My Position 3'),
+    ),
+    Marker(
+      markerId: MarkerId('Fourth'),
+      position: LatLng(52.2903, 21.0622),
+      infoWindow: InfoWindow(title: 'My Position 4'),
+    ),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    myMarker.addAll(markerList);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Nowa podróż'),
       ),
-      body: SizedBox(
-        height: 400,
+      body: SafeArea(
         child: GoogleMap(
           mapType: MapType.normal,
+          markers: Set<Marker>.of(myMarker),
           initialCameraPosition: _initialPosition,
           onMapCreated: (GoogleMapController controller) {
             _controller.complete(controller);
           },
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final GoogleMapController controller = await _controller.future;
+          controller.animateCamera(
+            CameraUpdate.newCameraPosition(
+              const CameraPosition(
+                target: LatLng(52.2903, 21.0622),
+                zoom: 12,
+              ),
+            ),
+          );
+          setState(() {});
+        },
+        child: Icon(Icons.add_location_alt_outlined),
       ),
     );
   }
---
+++ lib/features/newtrip/widgets/transform_latlng.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TransformLatlng extends StatefulWidget {
+  const TransformLatlng({super.key});
+
+  @override
+  State<TransformLatlng> createState() => _TransformLatlngState();
+}
+
+class _TransformLatlngState extends State<TransformLatlng> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
