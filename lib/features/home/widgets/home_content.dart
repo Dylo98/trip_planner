496ab9de6d3f41a5af3600f351e57472d18bf44f
Dylo98 <dawid.dyl@microsoft.wsei.edu.pl>
2025-05-23 19:07:41 +0200

    Add text animation animated_text_kit

--- lib/features/home/widgets/home_content.dart
+++ lib/features/home/widgets/home_content.dart
@@ -1,15 +1,46 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 
-class HomeContent extends StatelessWidget {
+class HomeContent extends StatefulWidget {
   const HomeContent({super.key});
 
   @override
+  State<HomeContent> createState() => _HomeContentState();
+}
+
+class _HomeContentState extends State<HomeContent> {
+  @override
   Widget build(BuildContext context) {
+    const colorizeColors = [
+      Colors.green,
+      Colors.blue,
+      Colors.yellow,
+      Colors.red,
+    ];
+
+    const colorizeTextStyle = TextStyle(
+      fontSize: 20,
+      fontFamily: 'Horizon',
+      fontWeight: FontWeight.bold,
+    );
+
     return Column(
       children: [
-        const Text(
-          'Witaj ponownie username!',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        SizedBox(
+          width: double.infinity,
+          child: Center(
+            child: AnimatedTextKit(
+              animatedTexts: [
+                ColorizeAnimatedText(
+                  'Witaj ponownie username!',
+                  colors: colorizeColors,
+                  textStyle: colorizeTextStyle,
+                ),
+              ],
+              isRepeatingAnimation: true,
+              repeatForever: true,
+            ),
+          ),
         ),
         const SizedBox(height: 20),
         const Text(
